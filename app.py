ca8f101df736a7ea4b6ae7ef0d215430acfd997c
cognicor-ai <51745262+cognicor-ai@users.noreply.github.com>
2019-06-12 17:10:03 +0530

    fixed indentation issue

--- app.py
+++ app.py
@@ -2,8 +2,8 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-    def hello():
-        return "Devops is not a goal, but a never ending process of continual improvement"
+def hello():
+    return "Devops is not a goal, but a never ending process of continual improvement"
 
 if __name__ == "__main__":
     app.run()
